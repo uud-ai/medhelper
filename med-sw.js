c2c8eef9cd2ba638a035cdd6a26d1629f3120e74
uud-ai <uudnik@gmail.com>
2026-03-08 15:27:56 +0900

    Add files via upload

---
+++ med-sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'medhelper-v1';
+const ASSETS = ['./', './index.html', './med-manifest.json', './med-icon-192.png', './med-icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
